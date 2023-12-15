27dee44d3ef8ffeb85cf7670544c3ef51b720500
HyeonGyu Lee <flewin@naver.com>
2023-12-15 12:05:31 +0900

    changed local const variables to macros

--- main.c
+++ main.c
@@ -1,30 +1,15 @@
 #include<stdio.h>
+#define MAX 					100	
+#define SECTOR_SIZE 			512	// 1 sector = 512 bytes
+#define SEEK_TIME				15 // ms
+#define ROTATE_LATENCY 			8 // ms
+#define TRANSFER_RATE			50 // Byte/ms
+#define COMMAND_OVERHEAD		30 // ms
+#define QUEUE_LENGTH			10000 // request count
+#define ERROR_RATE				(0.0001) // percentage
+#define CYCLE_POWER_CONSUMPTION	(0.01) // Watt
 
 int main(){
-	const int MAX = 100;
-	FILE *inputFile = fopen("workload.txt","r");
-	FILE *outputFile = fopen("result.txt", "w");
-
-
-	const int SECTOR_SIZE = 512; // 1 sector = 512 bytes
-    const int SEEK_TIME = 15; // ms
-    const int ROTATE_LATENCY = 8; // ms
-    const double TRANSFER_RATE = 50; // Byte/ms
-    const int COMMAND_OVERHEAD = 30; // ms
-    const int QUEUE_LENGTH = 10000; // request count
-    const double ERROR_RATE = 0.0001; // percentage
-    const double CYCLE_POWER_CONSUMPTION = 0.01; // W  
-
-
-	if(inputFile == NULL){
-		perror("ERROR: inputFile open failed");
-		return 1;
-	}
-	if(outputFile == NULL){
-		perror("ERROR: outputFile open failed");
-		fclose(inputFile);
-		return 1;
-	}
 	
 	char line[MAX];
 	char requestType;
@@ -36,6 +21,18 @@ int main(){
 	int totalTime = 0;
 	double totalPower = 0.0;
 
+	FILE *inputFile = fopen("workload.txt","r");
+	FILE *outputFile = fopen("result.txt", "w");
+
+	if(inputFile == NULL){
+		perror("ERROR: inputFile open failed");
+		return 1;
+	}
+	if(outputFile == NULL){
+		perror("ERROR: outputFile open failed");
+		fclose(inputFile);
+		return 1;
+	}
 	while(fgets(line, sizeof(line), inputFile) != NULL ){
 		sscanf(line, "%c %d %d",
 			   	&requestType, &logicalAddress, &sectorCount );
